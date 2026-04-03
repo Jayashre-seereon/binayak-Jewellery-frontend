6cc1c45aeb9878685ef6b95d0d07281edf46be5e
hriteshseereon <seereon.hritesh@gmail.com>
2026-04-03 11:46:47 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-amber-500">HeLLo ! Jwellery app design started</h1>
+    </>
+  )
+}
+
+export default App
